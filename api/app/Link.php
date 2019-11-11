545b40bc1034639cdee31f8b6e8850f107867575
Daryan <daryanka@hotmail.co.uk>
2019-11-11 23:24:37 +0000

    links model and migration and project_user rel table

---
+++ api/app/Link.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'project_id', 'link_name', 'link_url'
+    ];
+
+    protected $table = "links";
+    protected $primaryKey = "id";
+}
