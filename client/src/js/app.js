261917f0f5dc760aff07b86e590f7c19bf2de0ec
Daryan <daryanka@hotmail.co.uk>
2019-12-15 01:12:21 +0000

    making more changes to react boilerplate

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -1,10 +1,13 @@
 import React from "react"
 import "../scss/style.scss"
+import { Route, Switch } from "react-router-dom"
 
 const App = () => {
   return (
     <div>
-      <h1>testing 12345</h1>
+      <Switch>
+
+      </Switch>
     </div>
   )
 }
