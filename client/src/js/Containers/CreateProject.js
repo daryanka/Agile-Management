485aead779ce5fed89b3493d5468fa03a615d51b
Daryan <daryanka@hotmail.co.uk>
2020-02-03 21:50:49 +0000

    changed babelrc

--- client/src/js/Containers/CreateProject.js
+++ client/src/js/Containers/CreateProject.js
@@ -1,9 +1,37 @@
 import React from "react";
+import Form from "../Components/Form";
+import Input from "../Components/Input";
 
 const CreateProject = () => {
+  const [data, setData] = React.useState();
+
+  const handleChange = (val, name) => {
+    setData(prevState => {
+      return {...prevState, [name]: val}
+    })
+  };
+
+  const createProject = async () => {
+
+  }
+
   return(
     <div className={"create-project"}>
       <h1>Create New Project</h1>
+      <Form onSubmit={createProject}>
+        <Input
+          handleChange={handleChange}
+          name={"project_name"}
+          placeholder={""}
+          label={"Project Name*"}
+        />
+        <Input
+          handleChange={handleChange}
+          name={"project_name"}
+          placeholder={""}
+          label={"Project Name*"}
+        />
+      </Form>
     </div>
   )
 };
